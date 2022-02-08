0c59d5a197e6daeabbfaa4a3dcc4d92692c65bbf
Jonathon Nagy <nagy.jon93@gmail.com>
2022-02-08 22:34:45 +0100

    remove

--- 14.week/carProject/main.js
+++
@@ -1,6 +0,0 @@
-;document.getElementById('log').addEventListener('click',async () => {
-	const resp = await fetch('http://localhost:3000/api/cars')
-	const cars = await resp.json();
-
-	document.querySelector('pre').innerHTML = JSON.stringify(cars, null, 2)
-})
